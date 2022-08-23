0e225d876ab9b0b9638255467a8489a0be50c366
Ezra Kang <ezra.h.kang@gmail.com>
2022-08-23 16:51:54 -0400

    fixed js

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// Import the required DOM management plugins first
+import "dist/dom/data.js";
+import "dist/dom/event-handler.js";
+import "dist/dom/manipulator.js";
+import "dist/dom/selector-engine.js";
+import "dist/base-component.js";
+
+// Then your specific components
+import "dist/scrollspy.js";
